e7720e19dfda59ed0eaf606c94de8622a41cb2f6
ganature <498857839@qq.com>
2018-10-14 23:05:59 +0800

    '修改部分异常刚代码'

--- apps/RobotManager/views.py
+++ apps/RobotManager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from rest_framework import mixins
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
@@ -9,19 +9,19 @@ from django.forms.models import model_to_dict
 from rest_framework.views import View
 from django.urls import reverse
 from django.shortcuts import redirect
-from apps.RobotManager.serializers import ProjectSerializer,ModuleSerializer,SuitesSerializer, \
-    KeywordSerializer,UserKeywordsSerializer,CasesSerializer,CasesStepSerializer,StepswordsSerializer
+from apps.RobotManager.serializers import ProjectSerializer, ModuleSerializer, SuitesSerializer, \
+    KeywordSerializer, UserKeywordsSerializer, CasesSerializer, CasesStepSerializer, StepswordsSerializer
 import json
 
 # Create your views here.
 from apps.users.models import UserProfile
-from apps.RobotManager.models import Project,Module,Cases, \
-    Suites,Keyword,UserKeywords,Steps,Resource,CasesStep
-from apps.RobotManager.forms import ProjectForm,SuiteForm
+from apps.RobotManager.models import Project, Module, Cases, \
+    Suites, Keyword, UserKeywords, Steps, Resource, CasesStep
+from apps.RobotManager.forms import ProjectForm, SuiteForm
 from django.core.paginator import Paginator
 
 
-def get_pages(totalpage=1,current_page=1):
+def get_pages(totalpage=1, current_page=1):
     """
     example: get_pages(10,1) result=[1,2,3,4,5]
     example: get_pages(10,9) result=[6,7,8,9,10]
@@ -36,16 +36,16 @@ def get_pages(totalpage=1,current_page=1):
         behind_offset = front_offset - 1
 
     if totalpage < WEB_DISPLAY_PAGE:
-        return list(range(1,totalpage + 1))
+        return list(range(1, totalpage + 1))
     elif current_page <= front_offset:
-        return list(range(1,WEB_DISPLAY_PAGE + 1))
+        return list(range(1, WEB_DISPLAY_PAGE + 1))
     elif current_page >= totalpage - behind_offset:
         start_page = totalpage - WEB_DISPLAY_PAGE + 1
-        return list(range(start_page,totalpage + 1))
+        return list(range(start_page, totalpage + 1))
     else:
         start_page = current_page - front_offset
         end_page = current_page + behind_offset
-        return list(range(start_page,end_page + 1))
+        return list(range(start_page, end_page + 1))
 
 
 class ProjectView(View):
@@ -53,79 +53,88 @@ class ProjectView(View):
     项目列表页
     """
 
-    def get(self,request):
-        project_form=ProjectForm()
+    def get(self, request):
+        project_form = ProjectForm()
         project = Project.objects.all()
-        paginator_obj = Paginator(project,10)  # 每页10条
-        request_page_num = request.GET.get('page',1)
+        paginator_obj = Paginator(project, 10)  # 每页10条
+        request_page_num = request.GET.get('page', 1)
         project_obj = paginator_obj.page(request_page_num)
 
         total_page_number = paginator_obj.num_pages
 
-        project_list = get_pages(int(total_page_number),int(request_page_num))
+        project_list = get_pages(int(total_page_number), int(request_page_num))
 
-        return render(request,'robotTemplates/robot_project_list.html',
-                      {'obj': project_obj,'obj_list': project_list,'obj_form':project_form})
+        return render(request, 'robotTemplates/robot_project_list.html',
+                      {'obj': project_obj, 'obj_list': project_list, 'obj_form': project_form})
 
 
 class ProjectEditView(View):
     """
-    项目编辑页=
+    项目编辑页
     """
-    def get(self,request,id):
-        project = Project.objects.get(id=id)
-        return render(request,'robotTemplates/robot_project_edit.html',{'project': project})
 
-    def post(self,request):
+    def get(self, request, id):
+        project = Project.objects.get(id=id)
+        return render(request, 'robotTemplates/robot_project_edit.html', {'project': project})
+
+    def post(self, request):
         project_form = ProjectForm(request)
         if project_form.is_valid():
             project_form.save()
-        return render(request,'project_list.html')
+        return render(request, 'project_list.html')
 
 
 class ProjectAddView(View):
     """
     项目新增页面
     """
-    def get(self,request):
-        return render(request,'robotTemplates/robot_project_add.html')
 
-    def post(self,request):
+    def get(self, request):
+        return render(request, 'robotTemplates/robot_project_add.html')
+
+    def post(self, request):
         project_form = ProjectForm(request.POST)
         if project_form.is_valid():
             project_form.save()
         else:
-            return render(request,'robotTemplates/robot_project_add.html',{'error': project_form.errors})
+            return render(request, 'robotTemplates/robot_project_add.html', {'error': project_form.errors})
         return HttpResponseRedirect(reverse('project_list'))
+
+
 
 class SuiteAddView(View):
     """
     测试集新增页
     """
 
-    def get(self,request):
-        suiteform=SuiteForm()
-        return render(request,'robotTemplates/robot_suite_add.html',{'suiteform':suiteform})
+    @staticmethod
+    def get(request):
+        suiteform = SuiteForm()
+        return render(request, 'robotTemplates/robot_suite_add.html', {'suiteform': suiteform})
 
-    def post(self,request):
+    @staticmethod
+    def post(request):
         suiteform = SuiteForm(request.POST)
         if suiteform.is_valid():
             suiteform.save(commit=True)
-            return render(request,'robotTemplates/robot_suite_list.html',{'msg': '保存成功'})
+            return render(request, 'robotTemplates/robot_suite_list.html', {'msg': '保存成功'})
         else:
-            error=suiteform.errors
-            return render(request,'robotTemplates/robot_suite_add.html',{'suiteform':suiteform,'error': error})
+            error = suiteform.errors
+            return render(request, 'robotTemplates/robot_suite_add.html', {'suiteform': suiteform, 'error': error})
+
 
 class SuiteListView(View):
     """
     测试集列表页
     """
-    def get(self,request):
-        suite=Suites.objects.all()
-        paginator_obj=Paginator(suite,10)
-        request_page_num = request.GET.get('page',1)
-        suite_obj=paginator_obj.page(request_page_num)
-        total_page_number = paginator_obj.num_pages
-        suite_list=get_pages(int(total_page_number),int(request_page_num))
 
-        return render(request,'robotTemplates/robot_suite_list.html',{'obj':suite_obj,'obj_list':suite_list })
+    @staticmethod
+    def get(request):
+        suite = Suites.objects.all()
+        paginator_obj = Paginator(suite, 10)
+        request_page_num = request.GET.get('page', 1)
+        suite_obj = paginator_obj.page(request_page_num)
+        total_page_number = paginator_obj.num_pages
+        suite_list = get_pages(int(total_page_number), int(request_page_num))
+
+        return render(request, 'robotTemplates/robot_suite_list.html', {'obj': suite_obj, 'obj_list': suite_list})
